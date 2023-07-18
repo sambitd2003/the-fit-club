e98a1f3c0df66084eab32bb8fb20a32894e0cda4
Sambit Dutta <112713543+sambitd2003@users.noreply.github.com>
2023-07-18 23:08:03 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./Hero.css";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth <= 768 ? true : false;
+
+  return (
+    <div className="hero" id="home">
+      <div className="blur hero-blur"></div>
+      <div className="left-h">
+        <Header />
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <motion.div
+            initial={{ left: mobile ? "165px" : "238px" }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
+          <span>the best fitness club in the town</span>
+        </div>
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              Here we will help you to shape and build your ideal body and live
+              up your life to the fullest
+            </span>
+          </div>
+        </div>
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>
+              <CountUp end={150} start={100} duration="4" suffix="+" />
+            </span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>
+              <CountUp end={1000} start={800} duration="4" suffix="+" />
+            </span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>
+              <CountUp end={50} start={0} duration="4" suffix="+" />
+            </span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+        <motion.div
+          initial={{ right: "-1rem" }}
+          whileInView={{ right: "4rem" }}
+          transition={transition}
+          className="heart-rate"
+        >
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+        {/* hero images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <motion.img
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem" }}
+          transition={transition}
+          src={hero_image_back}
+          alt=""
+          className="hero-image-back"
+        />
+        {/* calories */}
+        <motion.div
+          initial={{ right: "37rem" }}
+          whileInView={{ right: "28rem" }}
+          transition={transition}
+          className="calories"
+        >
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/data/testimonialsData.js
@@ -0,0 +1,27 @@
+import image1 from "../assets/t-image1.jpeg";
+import image2 from "../assets/t-image2.jpeg";
+import image3 from "../assets/t-image3.jpeg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Fit Club and the right plan and program. I already achieved my ideal body!",
+    name: "RAHUL SAHA",
+    status: "ENTREPRENEUR"
+  },
+  {
+    image: image2,
+    review:
+      "I made the right choice by choosing the Fit Club and the right plan and program. I already achieved my ideal body!",
+    name: "SHIVAM SARKAR",
+    status: "COACH"
+  },
+  {
+    image: image3,
+    review:
+      "I made the right choice by choosing the Fit Club and the right plan and program. I already achieved my ideal body!",
+    name: "UTTIYA PAL",
+    status: "CUSTOMER"
+  }
+];
